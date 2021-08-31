6697a1ea03693362e594dc58b22b9b93a471a328
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-31 17:10:49 +0530

    added docker files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   base: '/TextUtils/',
   plugins: [reactRefresh()],
 })
